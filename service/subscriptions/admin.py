e95e6d21e84a870f0ac69dda6ca0940e09e69708
Mariya Polshkova <polshkovamamariya@gmail.com>
2025-04-27 21:17:22 +0300

    models structure created

--- service/subscriptions/admin.py
+++ service/subscriptions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Tariff, UserSubscription
 
-# Register your models here.
+
+admin.site.register(Tariff)
+admin.site.register(UserSubscription)
---
+++ service/subscriptions/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Tariff, UserSubscription
+
+class TariffSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tariff
+        fields = '__all__'
+
+class UserSubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserSubscription
+        fields = '__all__'
